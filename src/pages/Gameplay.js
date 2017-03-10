5d507af646fa203eff1f9ea8b92d75c0ca0a054f
Goh Horng Bor <infinitey@live.com>
2017-03-10 13:51:00 +0800

    Gameplay done

--- src/pages/Gameplay.js
+++ src/pages/Gameplay.js
@@ -21,17 +21,18 @@ export default class Gameplay extends React.Component {
     return (
 
       <div className="container" style={{marginTop: 20}}>
-        <img className="img-responsive col-md-8 col-md-offset-2" src="src/img/gameplay/title/web_title_system.png"/>
 
-        <hr className="featurette-divider"/>
+        <img className="img-responsive col-xs-12 col-sm-12 col-md-8 col-md-offset-2" src="src/img/gameplay/title/web_title_system.png"/>
 
-        <div className="featurette row" id="services">
-          <div className="row">
+        <hr className="featurette-divider" style={{backgroundColor:'transparent'}}/>
+
+        <div className="featurette row " id="services">
+          <div className="row hidden-xs hidden-sm">
             <div className="col-md-6 col-md-offset-1">
               <div className="row">
                 <img className="img-responsive col-md-10 pull-right " src="src/img/gameplay/title/web_title_battle.png"/>
               </div>
-              <div className="row">
+              <div className="row row hidden-xs hidden-sm" style={{marginTop:-40}}>
                 <TextPanel infotext={"Gameplay that combines SRPG with Tic Tac Toe; Warp and position your monsters across a 3x3 battlefield to attack enemies"} > </TextPanel>
               </div>
             </div>
@@ -41,66 +42,100 @@ export default class Gameplay extends React.Component {
             </div>
           </div>
 
+          <div className="row hidden-md hidden-lg" style={{margin:9, marginBottom:-30}}>
+            <img className="img-responsive col-sm-offset-1  col-xs-offset-1 col-sm-10  col-xs-10"  style={{zIndex: 999}} src="src/img/gameplay/title/web_title_battle.png"/>
+            <img className="img-responsive col-sm-offset-2  col-xs-offset-2 col-sm-8  col-xs-8" style={{marginTop:-50, zIndex: -999}} src="src/img/gameplay/web_image_battle_01.png"/>
+          </div>
+          <div className="row hidden-md hidden-lg col-sm-12  col-xs-12">
+            <TextPanel infotext={"Gameplay that combines SRPG with Tic Tac Toe; Warp and position your monsters across a 3x3 battlefield to attack enemies"}  />
+          </div>
         </div>
 
-        <hr className="featurette-divider"/>
+        <hr className="featurette-divider" style={{backgroundColor:'transparent'}}/>
 
 
         <div className="featurette" id="contact">
-          <div className="row">
-            <div className="col-md-5 col-md-offset-1">
-              <img className="featurette-image  img-responsive pull-left" src="src/img/gameplay/web_screenshot_chains_01.png"/>
+
+
+
+          <div className="row hidden-xs hidden-sm" style={{position: 'relative'}}>
+
+            <div className="col-xs-12 col-sm-12 col-md-5 col-md-offset-1">
+              <img className="img-responsive" src="src/img/gameplay/web_screenshot_chains_01.png"/>
             </div>
-            <div className="col-md-6">
-              <div className="row">
-                <img className="img-responsive col-md-8 pull-left " src="src/img/gameplay/title/web_title_chains.png"/>
+
+            <div className="col-md-offset-4 col-md-8 row"  style={{bottom:100, right:-100, position:'absolute'}}>
+
+              <div className="">
+                <img className="img-responsive col-md-8 pull-left " src="src/img/gameplay/title/web_title_chains.png" style={{bottom:-30}}/>
               </div>
-              <div className="row">
-                <TextPanel infotext={"Plan and execute strings of combos with your allies to unleash massive damage!"} />
+
+              <div className="row col-md-8">
+                <TextPanel infotext={"Plan and execute strings of combos with your allies to unleash massive damage!"}  />
               </div>
             </div>
           </div>
+
+          <div className="row hidden-md hidden-lg" style={{margin:9, marginBottom:-30}}>
+            <img className="img-responsive"  style={{zIndex: 999}} src="src/img/gameplay/title/web_title_chains.png"/>
+            <img className="img-responsive" style={{marginTop:-50, zIndex: -999}} src="src/img/gameplay/web_screenshot_chains_01.png"/>
+          </div>
+          <div className="row hidden-md hidden-lg col-sm-12  col-xs-12">
+            <TextPanel infotext={"Plan and execute strings of combos with your allies to unleash massive damage!"}  />
+          </div>
+
         </div>
 
-        <hr className="featurette-divider"/>
 
 
         <div className="featurette" id="services">
-          <div className="row">
-            <div className="col-md-5 col-md-offset-2">
-              <div className="row">
-                <img className="img-responsive col-md-8 pull-right " src="src/img/gameplay/title/web_title_feed.png"/>
+          <div className="row hidden-xs hidden-sm" >
+
+            <div className="col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1"  style={{position:'relative',marginTop:75}}>
+              <div className="row" style={{position:'absolute', top:30}}>
+                <img className="img-responsive col-md-8 pull-right" src="src/img/gameplay/title/web_title_story.png"/>
               </div>
-              <div className="row">
-                <TextPanel infotext={"Improve their stats and befriend them to unlock memorable conversations."} />
+              <div className="row " style={{marginTop:200}}>
+                <TextPanel infotext={"Experience the mystery behind 3 warring factions and their dark past. Step into the shoes of Indra on his journey of self-discovery and his pursuit of truth and justice as you complete the quests. Immerse yourself in an interactive tale of love and loss."} />
               </div>
             </div>
 
-            <div className="col-md-4">
-              <img className="featurette-image img-responsive pull-right" src="src/img/gameplay/web_image_feed_01.png"/>
+            <div className="col-md-5 col-lg-4" style={{marginLeft:-50}}>
+              <img className="featurette-image img-responsive pull-right" src="src/img/gameplay/web_screenshot_story_01.png"/>
             </div>
           </div>
 
+          <div className="row hidden-md hidden-lg" style={{margin:9, marginBottom:-30}}>
+            <img className="img-responsive col-sm-offset-1  col-xs-offset-1 col-sm-10  col-xs-10"  style={{marginBottom:-35}} src="src/img/gameplay/title/web_title_story.png"/>
+            <img className="img-responsive" style={{marginTop:-50, zIndex: -999}} src="src/img/gameplay/web_screenshot_story_01.png"/>
+          </div>
+          <div className="row hidden-md hidden-lg col-sm-12  col-xs-12">
+            <TextPanel infotext={"Experience the mystery behind 3 warring factions and their dark past. Step into the shoes of Indra on his journey of self-discovery and his pursuit of truth and justice as you complete the quests. Immerse yourself in an interactive tale of love and loss."}  />
+          </div>
 
         </div>
 
-
+        <hr className="featurette-divider" style={{backgroundColor:'transparent'}}/>
         <img className="img-responsive col-md-8 col-md-offset-2" src="src/img/gameplay/title/web_title_monster.png"/>
+        <hr className="featurette-divider" style={{backgroundColor:'transparent'}}/>
+
 
         <div className="" id="about">
           <div className="row" style={{position: 'relative'}}>
+            <img className="img-responsive col-xs-12 col-sm-12 pull-right hidden-md hidden-lg" style={{marginBottom:-30}} src="src/img/gameplay/title/web_title_collect.png"/>
             <img className="img-responsive col-xs-10 col-sm-10 col-md-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 " style={{}}  src="src/img/gameplay/web_image_evolve.png"/>
-            <img className="img-responsive col-xs-10 col-sm-8 col-md-8 pull-right" style={{position: 'absolute', right: 30, bottom: 0, margin:'auto'}} src="src/img/gameplay/title/web_title_collect.png"/>
+
+            <img className="img-responsive row hidden-xs hidden-sm col-md-8 pull-right" style={{position: 'absolute', right: 30, bottom: 0, margin:'auto'}} src="src/img/gameplay/title/web_title_collect.png"/>
           </div>
         </div>
 
-        <hr className="featurette-divider"/>
+
 
         <div className="featurette" id="services">
-          <div className="row">
+          <div className="row hidden-xs hidden-sm">
             <div className="col-md-5 col-md-offset-2">
               <div className="row">
-                <img className="img-responsive col-md-8 pull-right " src="src/img/gameplay/title/web_title_feed.png"/>
+                <img className="img-responsive col-md-8 pull-right " src="src/img/gameplay/title/web_title_feed.png" style={{marginBottom:-40}}/>
               </div>
               <div className="row">
                 <TextPanel infotext={"Improve their stats and befriend them to unlock memorable conversations."} />
@@ -108,33 +143,48 @@ export default class Gameplay extends React.Component {
             </div>
 
             <div className="col-md-4">
-              <img className="featurette-image img-responsive pull-right" src="src/img/gameplay/web_image_feed_01.png"/>
+              <img className=" img-responsive pull-right" src="src/img/gameplay/web_image_feed_01.png" style={{position:'absolute', left:-100}}/>
             </div>
           </div>
 
+          <div className="row hidden-md hidden-lg" style={{margin:9, marginBottom:-30}}>
+            <img className="img-responsive col-xs-offset-1 col-sm-offset-1 col-xs-10 col-sm-10"  style={{zIndex: 999}} src="src/img/gameplay/title/web_title_feed.png"/>
+            <img className="img-responsive col-xs-offset-1 col-sm-offset-1 col-xs-10 col-sm-10" style={{marginTop:-50, zIndex: -999}} src="src/img/gameplay/web_image_feed_01.png"/>
+          </div>
+          <div className="row hidden-md hidden-lg col-sm-12  col-xs-12" style={{marginTop:-10}}>
+            <TextPanel infotext={"Improve their stats and befriend them to unlock memorable conversations."}  />
+          </div>
 
         </div>
 
-        <hr className="featurette-divider"/>
+        <hr className="featurette-divider" style={{backgroundColor:'transparent'}}/>
 
 
-        <div className="featurette" id="contact">
-          <div className="row">
+        <div className="featurette" id="contact" style={{marginBottom:100}}>
+          <div className="row row hidden-xs hidden-sm">
             <div className="col-md-5 col-md-offset-1">
               <img className="featurette-image img-circle img-responsive pull-left" src="src/img/gameplay/web_custom_01.png"/>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6" style={{marginTop:80}}>
               <div className="row">
-                <img className="img-responsive col-md-10 pull-left " src="src/img/gameplay/title/web_title_custom.png"/>
+                <img className="img-responsive col-md-10 pull-left " src="src/img/gameplay/title/web_title_custom.png" style={{marginBottom:-30}}/>
               </div>
               <div className="row">
                 <TextPanel infotext={"Optimise your Monster party with over 250 skills. Equip and summon Puffs to aid you in battles."} />
               </div>
             </div>
           </div>
+
+          <div className="row hidden-md hidden-lg" style={{margin:9, marginBottom:-30}}>
+            <img className="img-responsive"  style={{zIndex: 999}} src="src/img/gameplay/title/web_title_custom.png"/>
+            <img className="img-responsive" style={{marginTop:-50, zIndex: -999}} src="src/img/gameplay/web_custom_01.png"/>
+          </div>
+          <div className="row hidden-md hidden-lg col-sm-12  col-xs-12" >
+            <TextPanel infotext={"Optimise your Monster party with over 250 skills. Equip and summon Puffs to aid you in battles."}  />
+          </div>
         </div>
 
-        <hr className="featurette-divider"/>
+
 
       </div>
     );
